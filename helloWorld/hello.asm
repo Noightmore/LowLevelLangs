d3279a9d2fa1610608fa2104c2f360503c4eb81a
john.doe <robert.hak@tul.cz>
2022-05-30 00:13:17 +0200

    /shrug

--- helloWorld/hello.asm
+++ helloWorld/hello.asm
@@ -1,21 +1,52 @@
-global _start
-
 ; to execute the program: ./hello
 
 section .data ; a used data section
-  message: db "Hello, world!", 0xA, 0x00 ; ten is the ascii code for new line, then null terminator
-  length equ $ - message
+  nameQuestionMSG db "What is your name? ", 10
+  greetByNameMSG db "Hello, "
 
-; text part - all code that is being executed
+section .bss
+    name resb 16  ; reserves bytes
+
 section .text
+    global _start
 
-_start:             
-    mov EAX, 4 ; sys_write function - more on official docs: http://asm.sourceforge.net/syscall.html
-    mov EBX, 1 ; file descriptor - first argument passed to the function
-    mov ECX, message ; char - second argument that gets passed to the function
-    mov EDX, length ; length - third argument that gets passed to the function
-    int 0x80
-    mov EAX, 1 ; sys_exit
-    mov EBX, 1
-    int 0x80
+    _start:
+        mov rax, nameQuestionMSG
+        call _printText
+        call _getUserName
+        mov rax, greetByNameMSG
+        call _printText
+        mov rax, name
+        call _printText
 
+        mov rax, 60
+        mov rdi, 0
+        syscall
+
+    _getUserName:
+        mov rax, 0
+        mov rdi, 0
+        mov rsi, name
+        mov rdx, 16
+        syscall
+        ret
+
+    ; input: rax is a pointer to string
+    ; output: prints string located at rax
+    _printText:
+        push rax
+        mov rbx, 0
+        ; doFor in assembly #komedie
+        _doFor:
+            inc rax
+            inc rbx
+            mov cl, [rax]
+            cmp cl, 0
+            jne _doFor
+        mov rax, 1
+        mov rdi, 1
+        pop rsi
+        mov rdx, rbx
+        syscall
+
+        ret
