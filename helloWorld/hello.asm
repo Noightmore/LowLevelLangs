09fc66ae36438e8a69e7737274cd0ad70415f245
john.doe <robert.hak@tul.cz>
2022-06-04 21:51:40 +0200

    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

--- helloWorld/hello.asm
+++ helloWorld/hello.asm
@@ -1,4 +1,8 @@
 ; to execute the program: ./hello
+; useful pastebin: https://pastebin.com/N1ZdmhLw
+
+%include "linux_consts.asm"
+%include "macros.asm"
 
 section .data ; a used data section
   nameQuestionMSG db "What is your name? ", 10, 0
@@ -11,17 +15,11 @@ section .text
     global _start
 
     _start:
-        mov rax, nameQuestionMSG
-        call _printText
+        print nameQuestionMSG
         call _getUserName
-        mov rax, greetByNameMSG
-        call _printText
-        mov rax, name
-        call _printText
-
-        mov rax, 60
-        mov rdi, 0
-        syscall
+        print greetByNameMSG
+        print name
+        exit ; a macro for exiting the program
 
     _getUserName:
         mov rax, 0
@@ -29,24 +27,4 @@ section .text
         mov rsi, name
         mov rdx, 16
         syscall
-        ret
-
-    ; input: rax is a pointer to string
-    ; output: prints string located at rax
-    _printText:
-        push rax
-        mov rbx, 0
-        ; doFor in assembly #komedie
-        _doFor:
-            inc rax
-            inc rbx
-            mov cl, [rax]
-            cmp cl, 0
-            jne _doFor
-        mov rax, 1
-        mov rdi, 1
-        pop rsi
-        mov rdx, rbx
-        syscall
-
         ret
