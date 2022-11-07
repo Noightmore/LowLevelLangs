a1d72280c1a655ecfafe17462a324969539c62d6
RH <robert.hak@tul.cz>
2022-11-07 20:51:47 +0000

    Delete a2/BUILD directory

--- a2/BUILD/DISCO_F746NG/ARMC6/mbed-os/platform/source/newlib_nano_malloc_workaround.d
+++
@@ -1,3 +0,0 @@
-BUILD/DISCO_F746NG/ARMC6/mbed-os/platform/source/newlib_nano_malloc_workaround.o: \
-  mbed-os/platform/source/newlib_nano_malloc_workaround.c \
-  BUILD/DISCO_F746NG/ARMC6/mbed_config.h
