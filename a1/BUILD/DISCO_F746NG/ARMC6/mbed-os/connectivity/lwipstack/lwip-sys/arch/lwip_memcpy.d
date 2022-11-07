bcbc85f2aa154242c1cf79fa94f64572d65f0ebf
RH <robert.hak@tul.cz>
2022-11-07 20:51:21 +0000

    Delete a1/BUILD directory

--- a1/BUILD/DISCO_F746NG/ARMC6/mbed-os/connectivity/lwipstack/lwip-sys/arch/lwip_memcpy.d
+++
@@ -1,3 +0,0 @@
-BUILD/DISCO_F746NG/ARMC6/mbed-os/connectivity/lwipstack/lwip-sys/arch/lwip_memcpy.o: \
-  mbed-os/connectivity/lwipstack/lwip-sys/arch/lwip_memcpy.c \
-  BUILD/DISCO_F746NG/ARMC6/mbed_config.h
--- a1/BUILD/DISCO_F746NG/ARMC6/mbed-os/hal/source/mbed_itm_api.d
+++
@@ -1,3 +0,0 @@
-BUILD/DISCO_F746NG/ARMC6/mbed-os/hal/source/mbed_itm_api.o: \
-  mbed-os/hal/source/mbed_itm_api.c \
-  BUILD/DISCO_F746NG/ARMC6/mbed_config.h
