3fcb6e42c9dd210fb686e4a585da96db2d2a2b74
robert <robert.hak@tul.cz>
2022-12-05 11:58:28 +0100

    random number generator is working yay!!!!!!

--- schoolTesting/random_seed_test.asm
+++ schoolTesting/random_seed_test.asm
@@ -2,20 +2,36 @@
 ; Link with (64 bit systems require elf_i386 option): ld random_seed_test.o -o random_seed_test
 ; Run with: ./random_seed_test
 
+
+; this is definitely not my idea, this algorithm is amazing! yay
+; source:
+; https://redirect.cs.umbc.edu/courses/undergraduate/CMSC211/fall01/burt/lectures/Chap14/random.html
+; rewritten from IBM pc assembly to x86_64 nasm assembly
+
 %include "print_functions.asm"
 
 section .data
-    seed:  dq 89
-    seed_len: dq 2
-    digits_vec: dq 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000
-    x86_64_ptr_byte_size equ 8 ; 64-bit pointers are 8 bytes
+    seed:  dd 8
+
+    A  equ	16807
+    S0 equ seed ;	Low order word of seed
+    S1 equ seed + 2 ;	High order word of seed
 
 section .text
     global _start
 
 _start:
 
-times 2 call gen_random_seed
+xor rax, rax
+_loop:
+    push rax
+    call gen_random_seed
+    mov rdi, [seed]
+    call print_num
+    pop rax
+    inc rax
+    cmp rax, 30
+    jne _loop
 
 ;exit
 mov rax, 60
@@ -23,96 +39,53 @@ mov rdi, 0
 syscall
 
 gen_random_seed:
- push rbp
-     mov rbp, rsp
+push rbp
+mov rbp, rsp
 
-     ; middle square method for generating random number by seed
+;
+;	P2|P1|P0 := (S1|S0) * A
+;
+mov	ax, [S0]
+mov	bx, A
+mul	bx
+mov	si, dx ;	si := pp01  (pp = partial product)
+mov	di, ax ;	di := pp00 = P0
+mov	ax, [S1]
+mul	bx ;	ax := pp11
+add	ax, si ;	ax := pp11 + pp01 = P1
+adc	dx, 0 ;	dx := pp12 + carry = P2
 
-     ; square seed
-     mov rax, [seed]
-     mul rax
-     push rax ; back up squared seed on stack
+;
+;	P2|P1:P0 = p * 2**31 + q, 0 <= q < 2**31
+;
+;	p = P2 SHL 1 + sign bit of P1 --> dx
+;		(P2:P1|P0 < 2**46 so p fits in a single word)
+;	q = (P1 AND 7FFFh)|P0 = (ax AND 7fffh) | di
+;
+shl	ax, 1
+rcl	dx, 1
+shr	ax, 1
+;
+;	dx:ax := p + q
+;
+add	dx, di ;	dx := p0 + q0
+adc	ax, 0 ;	ax := q1 + carry
+xchg ax, dx
+;
+;	if p+q < 2**31 then p+q is the new seed; otherwise whack
+;	  off the sign bit and add 1 and THATs the new seed
+;
+test	dx, 8000h
+jz	Store
+and	dx, 7fffh
+add	ax, 1 ;		inc doesn't set carry bit
+adc	dx, 0
 
-     ; loads trim divider-----------------
-     mov rax, [seed_len]
-     shr rax, 1 ; rax = rax / 2
-     dec rax
-     shl rax, 3 ; rdx = rdx * 8
-     add rax, digits_vec
-     mov rcx, [rax] ; get digit count by the amount of digits in the seed 1, 10, 100, 1000
-     ;-------------------------------------
- ;    push rcx
- ;    push rax
- ;
- ;    mov rdi, rcx
- ;    call print_num ; DEBUG
- ;
- ;    pop rax
- ;    pop rcx
+Store:
+	mov	[S1], dx
+	mov	[S0], ax
 
-     pop rax ;  load squared seed
-     div rcx ; rax = rax / rcx ; digits[trim]
-
-     push rcx
-     push rax
-
-     mov rdi, rax
-     call print_num ; DEBUG
-
-     pop rax
-     pop rcx
-
-     xor rbx, rbx ; clear rdx
-     xor r10, r10 ; clear r10 ; index of loop
-     _foreachDigit:
-         push rcx ; backup seed digit count
-         push rbx ; backup rbx
-         push rax ; backup squared seed
-
-         mov rdi, rax
-         mov rsi, rcx
-         call get_modulus
-         push rax ; backup the modulus product
-
-         ; compute the digit id by loop iteration
-         mov rax, r10
-         shl rax, 3 ; rax = rax * 8
-         add rax, digits_vec
-         mov rcx, [rax] ; digit count for current iteration
-
-         pop rax ; modulus product
-         mul rcx ; rax = rax * rcx
-         mov rcx, rax
-
-         pop rax ; squared seed
-         pop rbx ; final product
-         ; rbx = rbx + rcx ; final_val + (squared seed % digit count of seed) * digit count of current iteration
-         add rbx, rcx
-         pop rcx ; restore seed digit count
-
-         push rbx ; backup final product
-
- ;        push rax
- ;        push r10
- ;        push r11
- ;        push rcx
- ;        mov rdi, rax
- ;        call print_num
- ;        pop rcx
- ;        pop r11
- ;        pop r10
- ;        pop rax
-
-         mov rbx, 10
-         div rbx ; rax = rax / r11 == rax = rax / 10
-         pop rbx ; load back final product
-
-
-     inc r10 ; increment index of loop
-     cmp r10, [seed_len]
-     jnz _foreachDigit
-
-     leave
-     ret
+leave
+ret
 
 
